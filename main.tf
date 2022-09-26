ffc7dc2c9c39b55cde642d42599f2806897115d3
ATIF-EURUS <91880665+ATIF-EURUS@users.noreply.github.com>
2022-09-26 15:58:02 +0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,11 @@
-
-
+                                                                             main.tf                                                                                                
 variable "base_dir" {
   type        = string
   description = "The base directory in which this module will search for static files and templates."
-  Default `   = "Test"
+  default  = "Test"
 }
 
 output "tags_module" {
   description = "Tags Module in it's entirety"
-  value       = vars.base_dir
+  value       = var.base_dir
 }
